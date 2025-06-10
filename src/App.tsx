29040d70b93c0a66a70c7ede950488f490e0149a
sam <sam.k.baumann@gmail.com>
2025-06-10 07:53:49 -0400

    add more styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState, type FormEvent } from "react"
+import { useState, useRef, type FormEvent, useEffect } from "react"
 import './App.css'
 
 function App() {
   const [messageList, setMessageList] = useState<string[]>([])
   const [textboxVal, setTextboxVal] = useState<string>("")
   const [sendEnabled, setSendEnabled] = useState(true)
+  const messageEndRef = useRef<HTMLDivElement | null>(null)
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
@@ -23,16 +24,27 @@ function App() {
     }, 500)
   }
 
+  //scroll to end when a new message is sent
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }, [messageList])
+
   return (
     <>
-      <div className="max-w-[800px] mx-auto relative h-full">
-        {messageList.map((message, index) => {
-          return (
-            <div key={index}>
-              msg {index}: {message}
-            </div>
-          )
-        })}
+      <div className="max-w-[800px] mx-auto flex flex-col h-screen">
+        <div className="flex-1 overflow-y-auto p-4 w-full">
+          {messageList.map((message, index) => {
+            let isUser = index % 2 == 0
+            return (
+              <div className={`w-full flex ${isUser ? "justify-end" : "justify-start"}`}>
+                <div key={index} className={`w-fit max-w-[70%] rounded-lg px-3 py-2 ${isUser ? 'bg-blue-400' : 'bg-gray-200'}`}>
+                  {message}
+                </div>
+              </div>
+            )
+          })}
+          <div ref={messageEndRef} />
+        </div>
 
         <form onSubmit={handleSubmit} className="border p-2 rounded flex flex-col gap-2">
           <input type="text" onChange={(e) => setTextboxVal(e.target.value)} value={textboxVal}
